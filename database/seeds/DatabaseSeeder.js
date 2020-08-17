1d8b8082bcebdaa673dc8094876fb92daaf2528e
Benício Barreto <benicio.daniel@gmail.com>
2020-08-17 00:11:45 -0300

    Added ACL, Roles, Permissions. Edited Seeder

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -12,6 +12,9 @@
 
 const User = use('App/Models/User')
 
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
+
 class DatabaseSeeder {
   async run () {
     const user = await User.create({
@@ -20,11 +23,43 @@ class DatabaseSeeder {
       password: '123456'
     })
 
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Convidar membros'
+    })
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Criar projetos'
+    })
+
+    const admin = await Role.create({
+      slug: 'administrator',
+      name: 'Administrador'
+    })
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderador'
+    })
+
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitante'
+    })
+
+    await admin.permissions().attach([createInvite.id, createProject.id])
+    await moderator.permissions().attach([createProject.id])
+
     // creating automaticallly a team by relationship
-    await user.teams().create({
+    const team = await user.teams().create({
       name: 'Rocketman',
       user_id: user.id
     })
+
+    const teamJoin = await user.teamJoins().where('team_id', team.id).first()
+
+    await teamJoin.roles().attach([admin.id])
   }
 }
 
